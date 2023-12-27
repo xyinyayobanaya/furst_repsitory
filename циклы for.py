9569a4bfab6f40d366052e0fcf0b62853bfd7ca8
Alexsandr <154330370+xyinyayobanaya@users.noreply.github.com>
2023-12-27 17:44:03 +0300

    ДЗ

---
+++ циклы for.py
@@ -0,0 +1,10 @@
+cars = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for brand in cars:
+    print('Я езжу на автомабиле марки', brand)
+print('Выход из цикла')
+
+cars_count = 0
+for cars_count in range(10):
+    print(cars_count)
+
+
