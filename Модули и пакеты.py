19321045e4029b4072655ea52bb142ab4ae326c0
Alexsandr <154330370+xyinyayobanaya@users.noreply.github.com>
2024-02-08 16:30:41 +0300

    ДЗ

--- Модули и пакеты.py
+++ Модули и пакеты.py
@@ -1,6 +1,15 @@
 from main_1 import func1
 from main_2 import func2
 func1()
-func2()
+func2()`
 
+# main_1
+
+def func1():
+    print('Hello World')
+
+#main_2
+
+def func2():
+        print('hi peolple')
 
